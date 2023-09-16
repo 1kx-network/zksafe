4c3ef21a105135d778226a6bb125e4b4fee5cf09
Valeriy Zamaraiev <v@1kx.capital>
2023-09-16 09:50:31 +0200

    interim

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import "../circuits/contract/circuits/plonk_vk.sol";
+import "@safe-global/safe-contracts/contracts/base/Enum.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract ZkSafeModule {
+    UltraVerifier verifier;
+
+    function sendZkSafeTransaction(
+        // The Safe address to which the transaction will be sent.
+        address safe,
+
+        // The transaction data payload.
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+
+        // The proof data
+        bytes memory proof
+    ) public returns (bool) {
+
+        
+        
+        // Get the owners of the Safe by calling into the Safe contract.
+        return verifier.verifyProof(a, b, c, input);
     }
 
-    function increment() public {
-        number++;
-    }
 }
