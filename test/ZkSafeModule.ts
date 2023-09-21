e193574f0d23e1b388383cf08b5cb6e13b257579
Valeriy Zamaraiev <v@1kx.capital>
2023-09-21 10:41:47 +0200

    Basic tests with deployed contracts, no proofs yet

--- test/ZkSafeModule.ts
+++ test/ZkSafeModule.ts
@@ -1,23 +1,32 @@
 import { getSafeWithOwners } from "./setup";
 import hre, { ethers } from "hardhat";
 import { expect } from "chai";
+import { Safe, ZkSafeModule } from "../typechain-types";
+import { ContractFactory } from "ethers";
 
 describe("ZkSafeModule", function () {
     let signers = [];
-
-    let safe;;
+    let safe: Safe;
+    let zkSafeModule: ZkSafeModule;
 
     before(async function () {
         signers = (await ethers.getSigners()).slice(0, 10);
         // Deploy Safe
         let owners = signers.map((signer) => signer.address);
         console.log("owners", owners);
-        safe = await getSafeWithOwners();
+        safe = await getSafeWithOwners(owners, 3);
+
+        const verifierContractFactory = await ethers.getContractFactory("UltraVerifier");
+        const verifierContract = await verifierContractFactory.deploy();
+
+        const ZkSafeModule = await ethers.getContractFactory("ZkSafeModule");
+        zkSafeModule = await ZkSafeModule.deploy(verifierContract.getAddress());
+
     });
 
     it("Should fail to verify a nonexistent contract", async function () {
         const ZkSafeModule = await ethers.getContractFactory("ZkSafeModule");
-        const zkSafeModule = await ZkSafeModule.deploy("0x0000000000000000000000000000000000000000");
+        const zkSafeModule = await ZkSafeModule.deploy(safe.getAddress());
 
         const transaction  = {
             to: "0x0000000000000000000000000000000000000000",
@@ -25,11 +34,31 @@ describe("ZkSafeModule", function () {
             data: "0x",
             operation: 0,
         }
-    
+
         expect(zkSafeModule.sendZkSafeTransaction(
             "0x0000000000000000000000000000000000000000",
             transaction,
             "0x", // proof
         )).to.be.reverted;
     });
+
+    it("Should fail a bastic transaction with a wrong proof", async function () {
+        const ZkSafeModule = await ethers.getContractFactory("ZkSafeModule");
+        const zkSafeModule = await ZkSafeModule.deploy(safe.getAddress());
+
+        const transaction  = {
+            to: "0x0000000000000000000000000000000000000000",
+            value: 0,
+            data: "0x",
+            operation: 0,
+        }
+
+        expect(zkSafeModule.sendZkSafeTransaction(
+            safe.getAddress(),
+            transaction,
+            "0x", // proof
+        )).to.be.revertedWith("Invalid proof");
+    });
+
+
 });
